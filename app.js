dd53d38112bf5b8a08a64f0cea9d32365c32b7ff
Othon <othontheocharis@gmail.com>
2022-11-09 14:29:01 +0200

    First notCompleted Schema v1.0

---
+++ app.js
@@ -0,0 +1,6 @@
+//jshint esversion:6
+import listing from "./example/listing.js";
+
+console.log(listing);
+
+
---
+++ example/listing.js
@@ -0,0 +1,233 @@
+const listing = [ {
+
+    _id: "62a20062605b7e5b06b46d86",
+  
+    listing_id: 59872,
+  
+    owner: 8598,
+  
+    description: '',
+  
+    title: '5th floor Apartment with 1 bedroom in Syntagma',
+  
+    status: 'publish',
+  
+    modified_at: "2022-05-17T12:59:39.000Z",
+  
+    property_object_id: 58963,
+  
+    address: 'Mitropoleos 10, Syntagma, 10563',
+  
+    aircondition: 'yes',
+  
+    aircondition_number: '2',
+  
+    airy: 'yes',
+  
+    alarm: 'no',
+  
+    balcony: 'yes',
+  
+    balcony_size: '4 sqm',
+  
+    bathrooms: '1',
+  
+    bedrooms: '1',
+  
+    bright: 'yes',
+  
+    bus_distance: '85 m.',
+  
+    common_charges: null,
+  
+    construction_year: '1969',
+  
+    date_available_from: null,
+  
+    deposit: null,
+  
+    dishwasher: 'no',
+  
+    double_glazed_windows: 'no',
+  
+    elevator: 'yes',
+  
+    energy_class: 'g',
+  
+    equipped: 'no',
+  
+    estimated_bills: null,
+  
+    fireplace: 'no',
+  
+    floor: '5th',
+  
+    floor_plan: 'yes',
+  
+    floor_plan_image: '59889',
+  
+    floor_type: 'wood_mosaic',
+  
+    frames: 'wooden',
+  
+    fridge: 'no',
+  
+    full_address_gr: 'Μητροπόλεως 10, Σύνταγμα',
+  
+    furnished: 'no',
+  
+    gallery: '59881',
+  
+    garden: 'no',
+  
+    heat: 'autonomous',
+  
+    heat_source: 'aircondition',
+  
+    kitchens: '1',
+  
+    lease_duration: null,
+  
+    list_type: 'rent',
+  
+    listing_description: '1-bedroom apartment in the very heart of Athens, just a few steps from Syntagma Square. Partial renovation in 2022, which included: replacement of electric wiring, living room flooring polishing, fresh coat of paint in every room and installation of a security door. Exclusive access to a private terrace at the back of the building. The property is overlooking Mitropoleos Street and sits opposite a neoclassical building.',
+  
+    living_rooms: '1',
+  
+    lat: '37.975275',
+  
+    long: '23.7328859',
+  
+    metro_distance: '225 m. from Syntagma Station',
+  
+    neighborhood_description: null,
+  
+    oven: 'no',
+  
+    painted: 'yes',
+  
+    parking: 'no',
+  
+    parking_type: null,
+  
+    pet_friendly: 'yes',
+  
+    place_id: 'ChIJG9wMLTy9oRQReu78PppPShE',
+  
+    postcode: '10563',
+  
+    price: '850',
+  
+    price_sqm: '12.14',
+  
+    property_category: 'residence',
+  
+    property_extra_info: null,
+  
+    property_highlights: null,
+  
+    property_subcategory: 'apartment',
+  
+    renovated: 'no',
+  
+    renovated_details: 'Electric wiring changed,  floor in living room, painted every room, security door, ',
+  
+    renovation_year: '2022',
+  
+    satellite: 'no',
+  
+    sea_distance: '5,5 km',
+  
+    secured_door: 'yes',
+  
+    size: '70',
+  
+    solar_water_heater: 'no',
+  
+    spitogatos_broker_id: 'homi.one 59872',
+  
+    spitogatos_description_en: 'NO AGENT FEE - BOOK YOUR VIEWING DIRECTLY WITH THE LANDLORD AT homi.one/59872',
+  
+    spitogatos_description_gr: 'ΧΩΡΙΣ ΜΕΣΙΤΙΚΗ ΑΜΟΙΒΗ - ΚΛΕΙΣΤΕ ΡΑΝΤΕΒΟΥ ΞΕΝΑΓΗΣΗΣ ΑΠΕΥΘΕΙΑΣ ΜΕ ΤΟΝ ΙΔΙΟΚΤΗΤΗ ΣΤΟ homi.one/59872',
+  
+    spitogatos_id: '12561724',
+  
+    spitogatos_images: '59887',
+  
+    listing_status: 'available',
+  
+    storage_room: 'no',
+  
+    storage_room_description: null,
+  
+    stove: 'no',
+  
+    street_name: 'Mitropoleos',
+  
+    street_number: '10',
+  
+    suburb: 'Syntagma',
+  
+    tent: 'no',
+  
+    tv: 'no',
+  
+    underfloor_heat: 'no',
+  
+    video: null,
+  
+    view: 'Neoclassical building opposite',
+  
+    virtual_toor: '<iframe src="https://premium.giraffe360.com/homi/59872/?embed=true" width="800px" height="600px" allowfullscreen="true" style="border: 0;" scrolling="no"></iframe>',
+  
+    washing_machine: 'no',
+  
+    wc: null,
+  
+    window_screens: 'no',
+  
+    xe_description: 'ΧΩΡΙΣ ΜΕΣΙΤΙΚΗ ΑΜΟΙΒΗ - ΚΛΕΙΣΤΕ ΡΑΝΤΕΒΟΥ ΞΕΝΑΓΗΣΗΣ ΑΠΕΥΘΕΙΑΣ ΜΕ ΤΟΝ ΙΔΙΟΚΤΗΤΗ ΣΤΟ homi.one/59872',
+  
+    xe_id: 'HOMI_59872_1652783055',
+  
+    xe_images: '59881',
+  
+    picture: [
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL08.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL10.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL09.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL06.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL05.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL07.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL02.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL04.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL03.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL11.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL12.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/CAM00576P1-PR0103-STILL01.jpg',
+  
+      'https://homi.com.gr/wp-content/uploads/2022/05/floorplan01_00-crop.png'
+  
+    ],
+  
+    userId: "62a1fe05b3a87e072f87591b",
+  
+    propertyObjectId:"62a1fdc93ba081e5b48a5607"
+  
+  }]
+  
+  export default listing;
+  
+  
